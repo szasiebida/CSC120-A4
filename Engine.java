29e2ca282cd98229e1ed8caa169181a95b56ffaa
sofia <173196545+szasiebida@users.noreply.github.com>
2024-10-31 15:56:39 -0400

    engine class fix

--- Engine.java
+++ Engine.java
@@ -47,8 +47,8 @@ public class Engine {
      * function that refuels the engine by setting the current fuel level equal to the maximum
      * @param currentFuelLevel the amount of fuel currently in the engine 
      */
-    public void refuel(double currentFuelLevel){
-        currentFuelLevel=maxFuelLevel;
+    public void refuel(){
+        this.currentFuelLevel=this.maxFuelLevel;
     }
 
     /**
@@ -56,9 +56,9 @@ public class Engine {
      * @return boolean true if the fuel level is greater than 0 and false if it is less than 0 
      */
     public boolean go(){
-        if (currentFuelLevel>25){
-            currentFuelLevel=currentFuelLevel-25;
-            System.out.println("you have"+ currentFuelLevel+ "gallons of fuel left");
+        if (this.currentFuelLevel>25){
+            this.currentFuelLevel=this.currentFuelLevel-25;
+            System.out.println("you have"+ this.currentFuelLevel+ "gallons of fuel left");
             return true;
         } else{
             System.out.println("there is not enough fuel");
@@ -66,6 +66,10 @@ public class Engine {
         }
     }
 
+    /**
+     * main function for testing
+     * @param args
+     */
     public static void main(String[] args) {
         Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
         while (myEngine.go()) {
